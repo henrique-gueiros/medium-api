63dd24ecb8f00933264f07db60cdfee7d5703f3d
henrique-gueiros <riquewg@gmail.com>
2025-08-30 18:48:51 -0300

    feat: implement login and user existence check and error handling in createUser method

--- src/services/user.js
+++ src/services/user.js
@@ -8,6 +8,11 @@ export default class UserService {
         const transaction = await User.sequelize.transaction();
 
         try {
+
+            const userExists = await User.findOne({ where: { email: user.email, is_deleted: false } });
+            if (userExists) throw { status: 409, message: 'Email already in use' };
+
+
             user.password = await bcryptjs.hash(user.password, 10);
             const newUser = await User.create(user, { transaction });
 
@@ -20,4 +25,20 @@ export default class UserService {
         }
 
     } 
+
+    async login(email, password) {
+        try{
+            const user = await User.findOne({ where: { email, is_deleted: false } });
+
+            if (!user) throw { status: 401, message: "Invalid credentials" };
+
+            const senhaValida = await bcryptjs.compare(password, user.password);
+            if (!senhaValida) throw { status: 401, message: 'Invalid credentials' };
+
+            return pick(user, ['id', 'name', 'email', 'created_at', 'updated_at']);
+        } catch (error){
+            throw error;
+        }
+
+    }
 }
